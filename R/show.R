f16d89e8d3ff8d045697545b336124813f24600e
hosscine <futagamirentaro@gmail.com>
2018-03-30 13:51:37 +0900

    Add showImageList to show

--- R/show.R
+++ R/show.R
@@ -41,3 +41,43 @@ showImage <- function(img, ...){
   do.call(plot,args)
   graphics::rasterImage(img, 0, nr, nc, 0)
 }
+
+
+#' Plot many listed image at once.
+#'
+#' @param lst image list.
+#' @param row_col number of images that is plot at onece.
+#' if \code{row_col == 3}, 9 images plot as 3 times 3 layout.
+#' if \code{row_col == c(4,5)}, 20 images plot as 4 times 5 layout.
+#' @param plot.mar margins of each images.
+#' default is \code{c(2, 2, 2, 1), indicates top, left, bottom, right, respectively.}
+#' @param zero.mar if \code{T}, plot.mar is set as \code{c(0, 0, 0, 0)}.
+#' @param ... optional argments for plotting image.
+#'
+#' @importFrom assertthat is.count
+#' @importFrom myfs setMargin
+#' @importFrom myfs keywait
+#'
+#' @export
+#'
+showImageList <- function(lst, row_col = 3, plot.mar = c(2, 2, 2, 1), zero.mar = F, ...){
+  if(!is.count(row_col) && !(length(row_col ==2 && is.numeric(row_col))))
+    stop("row_col must be length 2 or 3 and be nature number")
+  if(length(row_col) == 1) row_col <- c(row_col, row_col)
+  par(mfrow = row_col)
+  if(zero.mar) setMargin(top.4 = 0, left.4 = 0, bottom.5 = 0, right.2 = 0)
+  else  setMargin(top.4 = plot.mar[1], left.4 = plot.mar[2],
+                  bottom.5 = plot.mar[3], right.2 = plot.mar[4])
+
+  i <- 0
+  n <- 1
+  repeat{
+    for (i in n:(n + prod(row_col) - 1)){
+      if(i > length(lst)) break
+      showImage(lst[[i]], ...)
+    }
+    if(i > length(lst)) break
+    n <- i
+    if(n != length(lst)) keywait()
+  }
+}
